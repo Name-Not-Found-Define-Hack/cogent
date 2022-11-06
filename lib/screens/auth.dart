c3c9cd3df8cc72c3eb7f69bfe439b01e2cfcc215
Gopal S <78960894+MaDGaMeR142@users.noreply.github.com>
2022-11-06 05:59:07 +0530

    added sign up screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bottom_sheet/bottom_sheet.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class Auth extends StatefulWidget {
   const Auth({super.key});
@@ -13,10 +14,14 @@ class _AuthState extends State<Auth> {
   var h = 100.0;
   var high = 0.45;
   var low = 0.05;
+  var obsec = true;
+
+  
 
   @override
   void initState() {
     Future.delayed(Duration(microseconds: 0), () {
+      Container bottomscreen = initialPage(context);
       h = (MediaQuery.of(context).size.height) * (44.5 / 100);
     });
     bottom();
@@ -26,10 +31,10 @@ class _AuthState extends State<Auth> {
   void bottom() {
     Future.delayed(Duration(milliseconds: 200), () {
       showFlexibleBottomSheet(
-        bottomSheetColor: Color(0xff252c3c),
+        bottomSheetColor: Colors.transparent,
         isDismissible: false,
         isCollapsible: false,
-        barrierColor: Color(0xff252c3c),
+        barrierColor: Colors.transparent,
         duration: Duration(milliseconds: 1500),
         minHeight: low,
         initHeight: high,
@@ -47,9 +52,9 @@ class _AuthState extends State<Auth> {
     double bottomSheetOffset,
   ) {
     return Material(
-        color: Color(0xff252c3c),
+        color: Colors.transparent,
         child: Container(
-            color: Color(0xff252c3c),
+            color: Colors.transparent,
             child: ListView(
                 controller: scrollController,
                 shrinkWrap: true,
@@ -61,7 +66,7 @@ class _AuthState extends State<Auth> {
   Container initialPage(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.grey,
+          color: Colors.transparent,
           borderRadius: BorderRadius.only(
               topRight: Radius.circular(50), topLeft: Radius.circular(50))),
       child: Column(
@@ -99,9 +104,10 @@ class _AuthState extends State<Auth> {
                     onPressed: () {
                       setState(() {
                         issignin = true;
-                        high = 0.65;
+                        high = 0.80;
+                        low = 0.45;
                         bottom();
-                        h = (MediaQuery.of(context).size.height) * (65 / 100);
+                        h = (MediaQuery.of(context).size.height) * (80 / 100);
                       });
                     },
                     child: Text(
@@ -180,12 +186,12 @@ class _AuthState extends State<Auth> {
                   ),
                 ),
                 SizedBox(
-                  height: 40,
+                  height: 30,
                 ),
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    "Sign in",
+                    "Sign up",
                     style: TextStyle(fontSize: 43.06),
                   ),
                 ),
@@ -195,13 +201,254 @@ class _AuthState extends State<Auth> {
                 Row(
                   children: [
                     Text("New user? "),
-                    TextButton(onPressed: () {}, child: Text("Create an account",style: TextStyle(color: Color(0xffff6177)),))
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Create an account",
+                          style: TextStyle(color: Color(0xffff6177)),
+                        ))
                   ],
                 ),
-               TextFormField()
+                TextFormField(
+                  cursorColor: Color(0xfffbc559),
+                  decoration: InputDecoration(hintText: "Email address"),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: SizedBox(
+                    width: 104,
+                    height: 41.19,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(fontSize: 13),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xfffbc559)),
+                          shape:
+                              MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ))),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                        height: 1.0,
+                        width: 130.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text("Or"),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                        height: 1.0,
+                        width: 130.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 57.82,
+                  width: 300,
+                  child: SignInButton(Buttons.Google,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 57.82,
+                  width: 300,
+                  child: SignInButton(Buttons.Facebook,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 57.82,
+                  width: 300,
+                  child: SignInButton(Buttons.Apple,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                )
               ],
             ),
-          )
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container enterpassword(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(50), topLeft: Radius.circular(50))),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(30),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                    topLeft: Radius.circular(50))),
+            height: h,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 24,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    width: 82,
+                    height: 27.64,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              "assets/logo_sign in.png",
+                            ),
+                            fit: BoxFit.fill)),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(fontSize: 43.06),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    Text("New user? "),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Create an account",
+                          style: TextStyle(color: Color(0xffff6177)),
+                        ))
+                  ],
+                ),
+                TextFormField(
+                  cursorColor: Color(0xfffbc559),
+                  decoration: InputDecoration(hintText: "Email address"),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: SizedBox(
+                    width: 104,
+                    height: 41.19,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(fontSize: 13),
+                      ),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xfffbc559)),
+                          shape:
+                              MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ))),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                        height: 1.0,
+                        width: 130.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text("Or"),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                        height: 1.0,
+                        width: 130.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 57.82,
+                  width: 300,
+                  child: SignInButton(Buttons.Google,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 57.82,
+                  width: 300,
+                  child: SignInButton(Buttons.Facebook,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 57.82,
+                  width: 300,
+                  child: SignInButton(Buttons.Apple,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50))),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -211,6 +458,7 @@ class _AuthState extends State<Auth> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff252c3c),
+      resizeToAvoidBottomInset: false,
     );
   }
 }
